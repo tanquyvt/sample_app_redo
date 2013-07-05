3eb520b3a46a0c45e4dcfe5dfbc0209e3acea6ee
tanquyvt <tanquyvt@gmail.com>
2013-07-05 10:54:38 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppRedo::Application.config.secret_token = 'e7b58a9b183eb3beb6f974c440a6e6434c1a23612150997eb02dba237980cc8cace28965044b546c75fe79ebb50eaefb3c5ba8d0b74fdc3d920699167aae52f8'
